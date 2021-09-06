e596748a93dc1386397b911b98c853cbf83e1df3
Ravinun Sintawarat <ravinun.s@kbtg.tech>
2021-09-06 14:16:42 +0700

    count all beans

--- src/main/java/com/example/demoapp/DemoappApplication.java
+++ src/main/java/com/example/demoapp/DemoappApplication.java
@@ -1,13 +1,27 @@
 package com.example.demoapp;
 
+import java.util.Arrays;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoappApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoappApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(DemoappApplication.class, args);
+		
+		//Beans
+		String[] names = context.getBeanDefinitionNames();
+		
+		for (String name : names) {
+			System.out.println(name);
+		}
+		
+		Arrays.stream(names).forEach(System.out::println);
+		
+		System.out.println(context.getBeanDefinitionCount());
 	}
 
 }
