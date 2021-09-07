bcbf3be8c3c5c121ef1a82c1cc903f8ad070ed64
Ravinun Sintawarat <ravinun.s@kbtg.tech>
2021-09-07 16:29:42 +0700

    Create user gateway and generate response class

---
+++ src/main/java/com/example/demoapp/InitializeData.java
@@ -0,0 +1,22 @@
+package com.example.demoapp;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+import com.example.demoapp.employees.Employee;
+import com.example.demoapp.employees.EmployeeRepository;
+
+@Configuration
+public class InitializeData {
+	
+	@Autowired
+	private EmployeeRepository employeeRepository;
+	
+//	@PostConstruct
+	public void xxxx() {
+		Employee employee = new Employee();
+		employee.setId(1000);
+		employee.setName("Pongsakorn");
+		employeeRepository.saveAndFlush(employee);
+	}
+}
